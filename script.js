9c8ed002ac0b87b2ec4b23ac57a6dbf711c71dd0
Piluca17 <b.piluca92@gmail.com>
2024-01-18 13:56:38 +0100

    Asociamos el listener a cada botón

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
+//Asociamos el listener a cada botón
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal)
+
+  btnCloseModal.addEventListener('click', closeModal)
+  overlay.addEventListener('click', closeModal)
+}
