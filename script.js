6a7d587ce8fb8290c0133deb6f1ff50568cef4a9
Piluca17 <b.piluca92@gmail.com>
2024-01-18 13:56:08 +0100

    Creadas las funciones

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+//Creamos las funciones
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
