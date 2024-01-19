5f93aa3467c9daf5a0971d69bf4decd0ab3ace32
Piluca17 <b.piluca92@gmail.com>
2024-01-19 11:49:49 +0100

    rsync realizado

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+
 //Creamos las funciones
+
 const openModal = function () {
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
@@ -14,14 +16,18 @@ const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
+
 //Asociamos el listener a cada botón
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal)
 
   btnCloseModal.addEventListener('click', closeModal)
   overlay.addEventListener('click', closeModal)
 }
+
 //Cerramos la ventana emergente con el botón ESC
+
 document.addEventListener('keydown', function (e) {
   // console.log(e, e.key);
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
